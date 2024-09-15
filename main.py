94632899d869f3bdedbd90e2eaa53983d1608f75
Bakhtiyorjon Dadajonov <98366569+bakhtiyorjondadajonov@users.noreply.github.com>
2024-09-15 13:17:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI,File,UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+app=FastAPI()
 import numpy as np
-from io import BytesIO
 from PIL import Image
+from io import BytesIO
 import tensorflow as tf
-
-app = FastAPI()
 
 origins = [
     "http://localhost",
@@ -22,34 +19,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("../saved_models/1")
-
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
 
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
+MODEL=tf.keras.models.load_model("./cnn_model.keras")
+CLASS_NAMES=['Ealy Blight',"Late Blight","Healthy"]
+
+def read_file_as_image(data)-> np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
     return image
-
-@app.post("/predict")
+@app.post("/predict")  
 async def predict(
-    file: UploadFile = File(...)
+    file: UploadFile=File(...)
 ):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
-    
-    predictions = MODEL.predict(img_batch)
+    image= read_file_as_image(await file.read())
+    image_batch=np.expand_dims(image,0)
+    predictions = MODEL.predict(image_batch)
+    result_ind=int(np.argmax(predictions[0]))
+    predicted_class=str(CLASS_NAMES[result_ind])
+    confidence=float(np.max(predictions[0]))
 
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
     return {
-        'class': predicted_class,
-        'confidence': float(confidence)
+        "class":predicted_class,
+        "confidence":confidence
     }
-
+# Run the server
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000)
-
+    uvicorn.run(app,host='localhost',port=8000)
